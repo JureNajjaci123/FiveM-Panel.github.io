50ccf88c464f1b05d1278f56deb2c3888e336ccd
JureNajjaci123 <103893893+JureNajjaci123@users.noreply.github.com>
2024-10-05 14:57:49 +0200

    Update config.php

--- config.php
+++ config.php
@@ -14,7 +14,7 @@
 	$GLOBALS['phpbin'] = "/opt/cpanel/ea-php72/root/usr/bin/lsphp"; 	// Used for Multithreading (Set to EXE Location for Windows) (Find Linux Bin via cPanel Info) (Defaulted to ELHostingServices)
 
 	// SITE SETTINGS
-	$GLOBALS['domainname'] = "https://fivempanel.com/";				// URL (and folder if used) with protocol and trailing slash. Example: https://arthurmitchell.xyz/beta/
+	$GLOBALS['domainname'] = "https://FiveM-Panel.github.io/";				// URL (and folder if used) with protocol and trailing slash. Example: https://arthurmitchell.xyz/beta/
 	$GLOBALS['subfolder'] = ""; 								// If accessing via a sub folder type the sub folder name out like the following: /foldername Example: /staff otherwise leave blank
 	$GLOBALS['apikey'] = "";								 			// SteamCommunity API Key https://steamcommunity.com/dev/apikey
 	date_default_timezone_set('America/New_York');						// Timezone (http://php.net/manual/en/timezones.php)
