3a1453873206ac0197abc4a1f2eb7a0bab57d3fd
JureNajjaci123 <103893893+JureNajjaci123@users.noreply.github.com>
2024-10-05 14:59:50 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,10 +5,10 @@
 
 
 	// DATABASE SETTINGS
-	$GLOBALS['mysql_host'] = "";										// MySQL Host
-	$GLOBALS['mysql_user'] = "";										// MySQL User
-	$GLOBALS['mysql_pass'] = "";										// MySQL Password
-	$GLOBALS['mysql_db'] = "";											// MySQL Database
+	$GLOBALS['mysql_host'] = "game.vibegames.com/";										// MySQL Host
+	$GLOBALS['mysql_user'] = "u55907_5OdIvODEPS";										// MySQL User
+	$GLOBALS['mysql_pass'] = "B3Uf4NMtBs46u0g6Xq3zWEMe";										// MySQL Password
+	$GLOBALS['mysql_db'] = "s55907_fivem";											// MySQL Database
 
 	// CRON SETTINGS
 	$GLOBALS['phpbin'] = "/opt/cpanel/ea-php72/root/usr/bin/lsphp"; 	// Used for Multithreading (Set to EXE Location for Windows) (Find Linux Bin via cPanel Info) (Defaulted to ELHostingServices)
